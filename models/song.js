a35eab31487dd228bc3207db0290a7ebb0b912b9
123Mike-ops <mikiyasdaniel983@gmail.com>
2024-08-26 21:30:47 +0300

    done

--- models/song.js
+++ models/song.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const songSchema=mongoose.Schema({
     title:{type:String},
     artist:{type:String},
-    albumId:{type:String},
+    album:{type:String},
     genre:{type:String},
     postedBy:{type:String},
     createTime:{type:Date,default:Date.now()},
